024e7474ade1d7a5adeda9f1ed10dc2a1e3a0b03
andresrocket <x88andres@hotmail.com>
2023-03-14 11:52:40 -0500

    Solved some target membership problems.

--- MarvelHeroes/MVP/Views/ViewExtensions/UIViewController+Utilities.swift
+++ MarvelHeroes/MVP/Views/ViewExtensions/UIViewController+Utilities.swift
@@ -15,7 +15,7 @@ extension UIViewController {
         let buttonTitle = NSLocalizedString("general.ok", comment: "general ok")
         let buttonAction = UIAlertAction(title: buttonTitle, style: .default, handler: nil)
         alertController.addAction(buttonAction)
-        DispatchQueue.main.async {
+        DispatchQueue.main.async { // In case this is called on background threads. 
             self.present(alertController, animated: true, completion: nil)
         }
     }
